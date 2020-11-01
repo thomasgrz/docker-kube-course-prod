f5882b83c8b1fb975d31c18f1e1357434dfae489
thomas <thomasgrz@outlook.com>
2020-11-01 13:15:43 -0500

    fix test

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1> Fib.js</h1>          
+          <h1> Fib</h1>          
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
